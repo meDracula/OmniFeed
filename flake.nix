e8f5c760d51196999cc80e172d1621d94487307c
meDracula <me.dracula@protonmail.com>
2024-10-06 16:44:52 +0200

    Nix Flake OmniFeed created with devShells

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "OmniFeed";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+  outputs = { self, nixpkgs }:
+  let
+    # Systems supported
+    allSystems = [
+      "x86_64-linux" # 64-bit Intel/AMD Linux
+      "aarch64-linux" # 64-bit ARM Linux
+    ];
+    # Helper to provide system-specific attributes
+    forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+      pkgs = import nixpkgs { inherit system; };
+    });
+  in
+  {
+    devShells = forallSystems ({ pkgs }: {
+      omnifeed = pkgs.mkShell {
+        name = "omnifeed";
+        packages = with pkgs; [
+          figlet
+          go # The Go CLI
+          gotools # Go tools like goimports, godoc, and others
+        ];
+        shellHook = ''echo "OmniFeed" | figlet'';
+      };
+    });
+  };
+}
